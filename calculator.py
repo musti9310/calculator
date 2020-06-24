62b94368b5eef5595a72bcf3832085cebdfff2db
Mustafa <musti9310@hotmail.com>
2020-06-24 22:06:11 +0300

    Task complete

--- calculator.py
+++ calculator.py
@@ -1,8 +1,38 @@
 
 def main():
-	#write your code here
-	pass
+	number1=input("Enter the first number:")
+	number2=input("Enter the second number:")
+	operation=input("Choose the operation (+, -, /, *):")
+
+	if number1.isnumeric() and number2.isnumeric():
+		number1 = float(number1)
+		number2 = float(number2)	
+		if operation == "+":
+			answer = number1 + number2
+			print("The answer is %f" %(answer))
+		elif operation == "-":
+			answer = number1 - number2
+			print("The answer is %f" %(answer)) 
+		elif operation == '/':
+			answer = number1 / number2
+			print("The answer is %f" %(answer))
+		elif operation == "*":
+			answer = number1 * number2
+			print("The answer is %f" %(answer))
+		else:
+			print('Invalid Input')
+
+				
+
+	else:
+		print("Invalid Input")
+
+
 	
+
+
+	
+				
 
 
 
